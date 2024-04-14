66998b0a2385276e9843ea7d70843b9399a79054
Nahom Abera <nahomtesfahun001@gmail.com>
2024-04-14 19:21:48 -0400

    Add Profiles page

---
+++ lib/profile_page.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ProfilePage extends StatefulWidget {
+  @override
+  _ProfilePageState createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  late User _user;
+  late TextEditingController _firstNameController;
+  late TextEditingController _lastNameController;
+  late TextEditingController _dobController;
+  late TextEditingController _phoneNumberController;
+  late TextEditingController _bioController;
+
+  @override
+  void initState() {
+    super.initState();
+    _user = _auth.currentUser!;
+    _firstNameController = TextEditingController();
+    _lastNameController = TextEditingController();
+    _dobController = TextEditingController();
+    _phoneNumberController = TextEditingController();
+    _bioController = TextEditingController();
+    _fetchProfileData();
+  }
+
+  Future<void> _fetchProfileData() async {
+    DocumentSnapshot userDoc =
+        await _firestore.collection('users').doc(_user.uid).get();
+    Map<String, dynamic> userData = userDoc.data() as Map<String, dynamic>;
+    _firstNameController.text = userData['firstName'];
+    _lastNameController.text = userData['lastName'];
+    _dobController.text = userData['dob'];
+    _phoneNumberController.text = userData['phoneNumber'];
+    _bioController.text = userData['bio'];
+  }
+
+  Future<void> _updateProfile() async {
+    try {
+      await _firestore.collection('users').doc(_user.uid).update({
+        'firstName': _firstNameController.text,
+        'lastName': _lastNameController.text,
+        'dob': _dobController.text,
+        'phoneNumber': _phoneNumberController.text,
+        'bio': _bioController.text,
+      });
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Profile updated successfully'),
+          backgroundColor: Colors.green,
+        ),
+      );
+    } catch (e) {
+      print('Error updating profile: $e');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('An error occurred. Please try again later.'),
+          backgroundColor: Colors.red,
+        ),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFF4A154B), // Slack's primary color
+        title: SizedBox(
+          height: AppBar().preferredSize.height,
+          child: Image.asset(
+            'assets/logo2.png', // Change to your logo file name
+            fit: BoxFit.contain,
+          ),
+        ),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.save),
+            onPressed: _updateProfile,
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextFormField(
+              controller: _firstNameController,
+              decoration: InputDecoration(labelText: 'First Name'),
+            ),
+            TextFormField(
+              controller: _lastNameController,
+              decoration: InputDecoration(labelText: 'Last Name'),
+            ),
+            TextFormField(
+              controller: _dobController,
+              decoration: InputDecoration(labelText: 'Date of Birth'),
+            ),
+            TextFormField(
+              controller: _phoneNumberController,
+              decoration: InputDecoration(labelText: 'Phone Number'),
+            ),
+            TextFormField(
+              controller: _bioController,
+              maxLines: 3,
+              decoration: InputDecoration(labelText: 'Bio'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _firstNameController.dispose();
+    _lastNameController.dispose();
+    _dobController.dispose();
+    _phoneNumberController.dispose();
+    _bioController.dispose();
+    super.dispose();
+  }
+}
