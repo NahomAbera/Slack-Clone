f6fc027a6649492bf97792a6d4cdf62ceab8ea44
Nahom Abera <nahomtesfahun001@gmail.com>
2024-04-14 12:07:31 -0400

    Update Registeration page

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'registeration_page.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -110,7 +111,7 @@ class _LoginPageState extends State<LoginPage> {
           SizedBox(height: 20.0),
           ElevatedButton(
             onPressed: _login,
-            child: Text('Login'),
+            child: Text('Login', style: TextStyle(color: Colors.white),),
             style: ElevatedButton.styleFrom(
               primary: Color(0xFF4A154B), 
             ),
@@ -157,153 +158,5 @@ class _LoginPageState extends State<LoginPage> {
         ),
       ),
     );
-  }
-}
-
-class RegistrationPage extends StatelessWidget {
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  TextEditingController _emailController = TextEditingController();
-  TextEditingController _passwordController = TextEditingController();
-  String _error = '';
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color(0xFF4A154B), 
-        title: Text('Slack Clone'),
-      ),
-      body: Column(
-        children: [
-          Container(
-            padding: EdgeInsets.symmetric(vertical: 20.0),
-            child: Image.asset(
-              'assets/logo.png', 
-              height: 200.0,
-              width: 400.0,
-            ),
-          ),
-          Expanded(
-            child: SingleChildScrollView(
-              child: Padding(
-                padding: EdgeInsets.all(16.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: <Widget>[
-                    Text(
-                      'Welcome to Slack',
-                      style: TextStyle(
-                        fontSize: 28.0,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                    SizedBox(height: 20.0),
-                    _buildRegistrationForm(),
-                    SizedBox(height: 20.0),
-                    _buildLoginLinkButton(context),
-                  ],
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildRegistrationForm() {
-    return Form(
-      key: _formKey,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          TextFormField(
-            controller: _emailController,
-            decoration: InputDecoration(
-              labelText: 'Email',
-              fillColor: Colors.white,
-              filled: true,
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-            ),
-            validator: (value) {
-              if (value?.isEmpty ?? true) {
-                return 'Please enter your email';
-              }
-              return null;
-            },
-          ),
-          SizedBox(height: 20.0),
-          TextFormField(
-            controller: _passwordController,
-            decoration: InputDecoration(
-              labelText: 'Password',
-              fillColor: Colors.white,
-              filled: true,
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-            ),
-            obscureText: true,
-            validator: (value) {
-              if (value?.isEmpty ?? true) {
-                return 'Please enter your password';
-              }
-              return null;
-            },
-          ),
-          SizedBox(height: 20.0),
-          Text(
-            _error,
-            style: TextStyle(color: Colors.red),
-          ),
-          SizedBox(height: 20.0),
-          ElevatedButton(
-            onPressed: _register,
-            child: Text('Register'),
-            style: ElevatedButton.styleFrom(
-              primary: Color(0xFF4A154B), 
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildLoginLinkButton(BuildContext context) {
-    return TextButton(
-      onPressed: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => LoginPage()),
-        );
-      },
-      child: Text(
-        'Already have an account? Login here',
-        style: TextStyle(
-          color: Colors.black,
-          fontWeight: FontWeight.bold,
-        ),
-      ),
-    );
-  }
-
-  void _register() async {
-    if (_formKey.currentState?.validate() ?? false) {
-      String email = _emailController.text.trim();
-      String password = _passwordController.text.trim();
-      try {
-        UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
-          email: email,
-          password: password,
-        );
-      } on FirebaseAuthException catch (e) {
-        _error = e.message ?? '';
-      }
-    }
   }
 }
---
+++ lib/registeration_page.dart
@@ -0,0 +1,326 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:image_picker/image_picker.dart';
+import 'login_page.dart';
+
+class RegistrationPage extends StatefulWidget {
+  @override
+  _RegistrationPageState createState() => _RegistrationPageState();
+}
+
+class _RegistrationPageState extends State<RegistrationPage> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+  TextEditingController _firstNameController = TextEditingController();
+  TextEditingController _lastNameController = TextEditingController();
+  TextEditingController _dobController = TextEditingController();
+  TextEditingController _phoneNumberController = TextEditingController();
+  TextEditingController _bioController = TextEditingController();
+  String _error = '';
+  File? _image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFF4A154B),
+        title: Text('Slack Clone'),
+      ),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 20.0),
+            child: Image.asset(
+              'assets/logo.png',
+              height: 200.0,
+              width: 400.0,
+            ),
+          ),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Text(
+                      'Welcome to Slack',
+                      style: TextStyle(
+                        fontSize: 28.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(height: 20.0),
+                    _buildRegistrationForm(),
+                    SizedBox(height: 20.0),
+                    _buildLoginLinkButton(context),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildRegistrationForm() {
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          TextFormField(
+            controller: _emailController,
+            decoration: InputDecoration(
+              labelText: 'Email*',
+              fillColor: Colors.white,
+              filled: true,
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+            ),
+            validator: (value) {
+              if (value?.isEmpty ?? true) {
+                return 'Please enter your email';
+              }
+              return null;
+            },
+          ),
+          SizedBox(height: 20.0),
+          TextFormField(
+            controller: _passwordController,
+            decoration: InputDecoration(
+              labelText: 'New Password*',
+              fillColor: Colors.white,
+              filled: true,
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+            ),
+            obscureText: true,
+            validator: (value) {
+              if (value?.isEmpty ?? true) {
+                return 'Please enter your password';
+              }
+              return null;
+            },
+          ),
+          SizedBox(height: 20.0),
+          TextFormField(
+            controller: _firstNameController,
+            decoration: InputDecoration(
+              labelText: 'First Name*',
+              fillColor: Colors.white,
+              filled: true,
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+            ),
+            validator: (value) {
+              if (value?.isEmpty ?? true) {
+                return 'Please enter your first name';
+              }
+              return null;
+            },
+          ),
+          SizedBox(height: 20.0),
+          TextFormField(
+            controller: _lastNameController,
+            decoration: InputDecoration(
+              labelText: 'Last Name*',
+              fillColor: Colors.white,
+              filled: true,
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+            ),
+            validator: (value) {
+              if (value?.isEmpty ?? true) {
+                return 'Please enter your last name';
+              }
+              return null;
+            },
+          ),
+          SizedBox(height: 20.0),
+          TextFormField(
+            controller: _dobController,
+            readOnly: true,
+            decoration: InputDecoration(
+              labelText: 'Date of Birth*',
+              fillColor: Colors.white,
+              filled: true,
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              suffixIcon: IconButton(
+                icon: Icon(Icons.calendar_today),
+                onPressed: () async {
+                  final DateTime? pickedDate = await showDatePicker(
+                    context: context,
+                    initialDate: DateTime.now(),
+                    firstDate: DateTime(1900),
+                    lastDate: DateTime.now(),
+                  );
+                  if (pickedDate != null) {
+                    setState(() {
+                      _dobController.text = pickedDate.toString().split(' ')[0];
+                    });
+                  }
+                },
+              ),
+            ),
+            validator: (value) {
+              if (value?.isEmpty ?? true) {
+                return 'Please select your date of birth';
+              }
+              return null;
+            },
+          ),
+          SizedBox(height: 20.0),
+          TextFormField(
+            controller: _phoneNumberController,
+            decoration: InputDecoration(
+              labelText: 'Phone Number (optional)',
+              fillColor: Colors.white,
+              filled: true,
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+            ),
+          ),
+          SizedBox(height: 20.0),
+          TextFormField(
+            controller: _bioController,
+            decoration: InputDecoration(
+              labelText: 'Bio (optional)',
+              fillColor: Colors.white,
+              filled: true,
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+            ),
+            maxLines: 3,
+          ),
+          SizedBox(height: 20.0),
+          ElevatedButton(
+            onPressed: () {
+              _getImage();
+            },
+            child: Text(
+              'Select Profile Picture (optional)',
+              style: TextStyle(color: Colors.white),
+            ),
+            style: ElevatedButton.styleFrom(
+              primary: Color(0xFF4A154B),
+            ),
+          ),
+          SizedBox(height: 20.0),
+          _image != null
+              ? Image.file(
+                  _image!,
+                  height: 200.0,
+                  width: 200.0,
+                  fit: BoxFit.cover,
+                )
+              : SizedBox(),
+          SizedBox(height: 20.0),
+          ElevatedButton(
+            onPressed: _register,
+            child: Text('Register', style: TextStyle(color: Colors.white),),
+            style: ElevatedButton.styleFrom(
+              primary: Color(0xFF4A154B),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildLoginLinkButton(BuildContext context) {
+    return TextButton(
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => LoginPage()),
+        );
+      },
+      child: Text(
+        'Already have an account? Login here',
+        style: TextStyle(
+          color: Colors.black,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+
+  void _register() async {
+    if (_formKey.currentState?.validate() ?? false) {
+      String email = _emailController.text.trim();
+      String password = _passwordController.text.trim();
+      String firstName = _firstNameController.text.trim();
+      String lastName = _lastNameController.text.trim();
+      String dob = _dobController.text.trim();
+      String phoneNumber = _phoneNumberController.text.trim();
+      String bio = _bioController.text.trim();
+      String imageUrl = await _uploadImageToStorage();
+
+      try {
+        UserCredential userCredential =
+            await _auth.createUserWithEmailAndPassword(
+          email: email,
+          password: password,
+        );
+
+        await FirebaseFirestore.instance
+            .collection('users')
+            .doc(userCredential.user!.uid)
+            .set({
+          'email': email,
+          'firstName': firstName,
+          'lastName': lastName,
+          'dob': dob,
+          'phoneNumber': phoneNumber,
+          'bio': bio,
+          'profilePicture': imageUrl,
+        });
+      } on FirebaseAuthException catch (e) {
+        _error = e.message ?? '';
+      }
+    }
+  }
+
+  Future<String> _uploadImageToStorage() async {
+    if (_image != null) {
+      Reference storageReference = FirebaseStorage.instance
+          .ref()
+          .child('profile_pictures/${DateTime.now().millisecondsSinceEpoch}');
+      UploadTask uploadTask = storageReference.putFile(_image!);
+      await uploadTask.whenComplete(() => null);
+      String imageUrl = await storageReference.getDownloadURL();
+      return imageUrl;
+    } else {
+      return '';
+    }
+  }
+
+  Future<void> _getImage() async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.getImage(source: ImageSource.gallery);
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+      } else {
+        print('No image selected.');
+      }
+    });
+  }
+}
